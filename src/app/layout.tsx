406da481dc05c5ef0d3efea0a4e9fdfa869ff26b
Anurag Gupta <anurag6967@gmail.com>
2024-06-24 17:39:08 +0530

    Music Application using NextJs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+
+         <h2 className="text-center font-bold bg-green-400">NavBar of home page </h2>
+         <Navbar />
+
+         {children}
+        
+      </body>
     </html>
   );
 }
