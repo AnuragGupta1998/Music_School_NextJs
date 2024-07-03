c7a66d9a7881291f78ffd9d19b2fcea521a3bd3a
Anurag Gupta <anurag6967@gmail.com>
2024-07-03 21:38:26 +0530

    replace <img> by <Image>

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react'
 import Image from "next/image";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card"
@@ -41,59 +43,62 @@ function page() {
             </h1>
 
             {/* <div className=" flex flex-wrap justify-center gap-8"> */}
-            <div className=" grid md:grid-cols-3">
 
+            <div className=" grid md:grid-cols-3">
                 {courseData.courses.map((course: Courses) => (
 
-                    <CardContainer className="inter-var m-4">
-                        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                    <div key={course.id}>
 
-                            <CardItem
-                                translateZ="50"
-                                className="text-xl font-bold text-neutral-600 dark:text-white"
-                            >
-                                {course.title}
-                            </CardItem>
-
-                            <CardItem
-                                as="p"
-                                translateZ="60"
-                                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-                            >
-                                {course.description}
-                            </CardItem>
-
-                            <CardItem translateZ="100" className="w-full mt-4">
-                                <Image
-                                    src={course.image}
-                                    width="100"
-                                    height="50"
-                                    className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                                    alt={course.title}
-                                />
-                            </CardItem>
-
-                            <div className="flex justify-between items-center mt-20">
+                        <CardContainer className="inter-var m-4">
+                            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
 
                                 <CardItem
-                                    translateZ={20}
-                                    as="button"
-                                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                                    translateZ="50"
+                                    className="text-xl font-bold text-neutral-600 dark:text-white"
                                 >
-                                    Try now →
+                                    {course.title}
                                 </CardItem>
 
                                 <CardItem
-                                    translateZ={20}
-                                    as="button"
-                                    className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                                    as="p"
+                                    translateZ="60"
+                                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
                                 >
-                                    Sign up
+                                    {course.description}
                                 </CardItem>
 
-                            </div>
-                        </CardBody>
-                    </CardContainer>
+                                <CardItem translateZ="100" className="w-full mt-4">
+                                    <Image
+                                        src={course.image}
+                                        width="100"
+                                        height="50"
+                                        className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                        alt={course.title}
+                                    />
+                                </CardItem>
+
+                                <div className="flex justify-between items-center mt-20">
+
+                                    <CardItem
+                                        translateZ={20}
+                                        as="button"
+                                        className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                                    >
+                                        Try now →
+                                    </CardItem>
+
+                                    <CardItem
+                                        translateZ={20}
+                                        as="button"
+                                        className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+                                    >
+                                        Sign up
+                                    </CardItem>
+
+                                </div>
+                            </CardBody>
+                        </CardContainer>
+                    </div>
 
                 ))}
 
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -44,19 +44,25 @@ function FeaturedCourses() {
                                 <BackgroundGradient
                                     className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm"
                                 >
-                                     <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                                    <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
 
                                         <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
 
                                         <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
 
-                                        <img src={course.image} className='h-[250px] object-cover'></img>
+                                        <Image
+                                            src={course.image}
+                                            width="100"
+                                            height="50"
+                                            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                                            alt={course.title}
+                                        />
 
                                         <Link href={`/courses/${course.slug}`}>
                                             Learn More
                                         </Link>
 
-                                    </div> 
+                                    </div>
 
                                 </BackgroundGradient>
 
